8ecd7df19a8d9b2d59808e7d01b903011111720f
lown <lown_xz@proton.me>
2026-03-22 21:04:38 +0700

    [fix] Dev mode default OFF, settings cooldown now respected

--- common/src/main/kotlin/notlown/cobblebase/core/CobblebaseConfig.kt
+++ common/src/main/kotlin/notlown/cobblebase/core/CobblebaseConfig.kt
@@ -6,7 +6,7 @@ package notlown.cobblebase.core
  */
 object CobblebaseConfig {
     // Dev mode: reduces all cooldowns to 1 second for testing
-    var devMode = true // TODO: set to false for release
+    var devMode = false
 
     // Passive XP
     var passiveXpEnabled = true

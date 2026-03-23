df92b3674b2fb7096727b5c8e7ef0db4a36b692b
lown <lown_xz@proton.me>
2026-03-23 21:36:36 +0700

    [add] K hotkey to open Cobblebase settings
    - Press K in-game to open settings directly
    - Rebindable in Minecraft Controls menu under "Cobblebase" category
    - No more searching through mod list

--- fabric/src/main/kotlin/notlown/cobblebase/fabric/client/CobblebaseFabricClient.kt
+++ fabric/src/main/kotlin/notlown/cobblebase/fabric/client/CobblebaseFabricClient.kt
@@ -1,8 +1,33 @@
 package notlown.cobblebase.fabric.client
 
+import me.shedaniel.autoconfig.AutoConfig
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
+import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.util.InputUtil
+import notlown.cobblebase.core.CobblebaseClothConfig
+import org.lwjgl.glfw.GLFW
 
 object CobblebaseFabricClient : ClientModInitializer {
+
+    private lateinit var settingsKey: KeyBinding
+
     override fun onInitializeClient() {
+        // Register keybinding: K = open Cobblebase settings
+        settingsKey = KeyBindingHelper.registerKeyBinding(KeyBinding(
+            "key.cobblebase.settings",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_K,
+            "Cobblebase"
+        ))
+
+        // Listen for key press
+        ClientTickEvents.END_CLIENT_TICK.register { client ->
+            if (settingsKey.wasPressed()) {
+                val screen = AutoConfig.getConfigScreen(CobblebaseClothConfig::class.java, client.currentScreen).get()
+                client.setScreen(screen)
+            }
+        }
     }
 }
